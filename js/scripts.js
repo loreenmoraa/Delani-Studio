4d71cf689347f5c384c329301eab8c767c0eb86b
Loreen <loreenmoraa@gmail.com>
2022-01-20 17:37:32 +0300

    modified code

--- js/scripts.js
+++ js/scripts.js
@@ -89,7 +89,7 @@ $(document).ready(function(){
     var name = $("input#MERGE1").val();
     var email = $("input#MERGE2").val();
     var message = $("textarea#comment").val();
-    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+    if ($("input#MERGE1").val() && $("input#MERGE2").val()){
       alert (name + ", we have received your message. Thank you for reaching out to us.");
     }
     else {
