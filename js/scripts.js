07c35540e3d76d7198d8817519f4d79e29ad970d
Loreen <loreenmoraa@gmail.com>
2022-01-20 16:53:40 +0300

    added more code to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,25 @@ $(document).ready(function(){
     $("#design-image").slideDown('1500');
   });
 });
+   
+$(document).ready(function(){
+  $("#development-image").click(function(){
+    $("#development-image").slideDown('1500').hide('1000');
+    $("#development").show('1500');
+  });
+  $("#development").click(function(){
+    $("#development").slideUp('1500');
+    $("#development-image").slideDown('1500');
+  });
+});
+
+$(document).ready(function(){
+  $("#product-image").click(function(){
+    $("#product-image").slideDown('1500').hide('1000');
+    $("#product").show('1500');
+  });
+  $("#product").click(function(){
+    $("#product").slideUp('1500');
+    $("#product-image").slideDown('1500');
+  });
+});
